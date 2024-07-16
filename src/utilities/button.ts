e777b00c4da339b228c5d018fc8301eb956efcba
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2024-07-16 11:04:05 -0600

    feat: create utility for getting button variant

--- src/utilities/button.ts
+++ src/utilities/button.ts
@@ -3,8 +3,23 @@ import { ButtonSize, ButtonVariant } from "../components";
 
 interface ButtonClassParams {
   size?: ButtonSize;
-  variant?: ButtonVariant;
+  variant: ButtonVariant;
   outlined?: boolean;
+}
+
+export function getButtonVariant(variant: ButtonVariant) {
+  return clsx({
+    "ui-btn-primary": variant === "primary",
+    "ui-btn-secondary": variant === "secondary",
+    "ui-btn-accent": variant === "accent",
+    "ui-btn-info": variant === "info",
+    "ui-btn-success": variant === "success",
+    "ui-btn-warning": variant === "warning",
+    "ui-btn-error": variant === "error",
+    "ui-btn-ghost": variant === "ghost",
+    "ui-btn-link": variant === "link",
+    "ui-btn-neutral": variant === "neutral",
+  });
 }
 
 /** Create the styling for a custom component that has button-like appearance */
@@ -15,18 +30,7 @@ export function getButtonClasses({
 }: ButtonClassParams) {
   return clsx(
     "ui-btn",
-    {
-      "ui-btn-primary": variant === "primary",
-      "ui-btn-secondary": variant === "secondary",
-      "ui-btn-accent": variant === "accent",
-      "ui-btn-info": variant === "info",
-      "ui-btn-success": variant === "success",
-      "ui-btn-warning": variant === "warning",
-      "ui-btn-error": variant === "error",
-      "ui-btn-ghost": variant === "ghost",
-      "ui-btn-link": variant === "link",
-      "ui-btn-neutral": variant === "neutral",
-    },
+    getButtonVariant(variant),
     {
       "ui-btn-xs": size === "xs",
       "ui-btn-sm": size === "sm",
