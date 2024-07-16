8a5876f8c83a00abac431856e9d3e17bd243df9d
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2024-07-16 11:04:31 -0600

    feat: add fade animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   prefix: "ui-",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", filter: "blur(5px)" },
+          "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "100%": {
+            opacity: "0",
+            filter: "blur(5px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-in-out",
+        "fade-out": "fade-out 0.3s ease-in-out",
+      },
+    },
   },
   daisyui: {
     themes: THEME_LIST,
