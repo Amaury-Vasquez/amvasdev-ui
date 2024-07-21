dcbbfbc19eddd1578b295c51f2ce1547b6eebe55
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2024-07-20 19:39:20 -0600

    feat: export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,12 +6,23 @@ export {
   ButtonType,
   ButtonVariant,
 } from "./Button";
+export { default as Checkbox, CheckboxProps } from "./Checkbox";
 export { default as ColorPalette, ColorPaletteProps } from "./ColorPalette";
 export {
   default as Dropdown,
   DropdownProps,
   DropdownPosition,
 } from "./Dropdown";
+export { default as ErrorLabel, ErrorLabelProps } from "./ErrorLabel";
 export { default as IconButton, IconButtonProps } from "./IconButton";
 export { default as Input, InputProps, InputSize, InputVariant } from "./Input";
+export { default as Label, LabelProps } from "./Label";
 export { default as Modal, ModalProps } from "./Modal";
+export {
+  default as RadioGroup,
+  RadioGroupProps,
+  RadioGroupOrientation,
+  RadioGroupSize,
+  RadioGroupVariant,
+  RadioOption,
+} from "./RadioGroup";
