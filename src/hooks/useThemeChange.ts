5bfbc4afbdb2477fb6ec62578aa6b3e249204958
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2024-08-04 18:10:42 -0600

    feat: remove callback from hook

--- src/hooks/useThemeChange.ts
+++ src/hooks/useThemeChange.ts
@@ -1,12 +1,11 @@
 import { useCallback } from "react";
 import { THEME_LIST } from "../themes";
 
-const useThemeChange = (onFinishChange = () => {}) => {
+const useThemeChange = () => {
   const changeTheme = useCallback((theme: (typeof THEME_LIST)[number]) => {
     const root = document.querySelector("html");
     if (root) {
       root.setAttribute("data-theme", theme);
-      onFinishChange();
     }
   }, []);
 
