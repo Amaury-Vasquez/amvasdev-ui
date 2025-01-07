0d13e308ac646f21e1dc3990a2dea469c2314d1c
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2025-01-06 21:36:56 -0600

    fix: tooltip color

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -37,20 +37,18 @@ const IconButton = ({
         <span className="ui-absolute ui-left-1/2 ui-right-1/2 -ui-translate-x-1/2 ui-bottom-[calc(100%+0.5rem)] ui-w-fit">
           <span
             className={clsx(
-              "ui-bg-base-content ui-rounded-lg ui-truncate ui-py-2 ui-px-3 ui-w-fit ui-shadow-sm ui-h-fit",
+              "ui-bg-base-200 ui-rounded-lg ui-truncate ui-py-2 ui-px-3 ui-w-fit ui-shadow-sm ui-h-fit",
               "ui-items-center ui-justify-center ui-flex ui-relative"
             )}
           >
             {typeof tooltip === "string" ? (
-              <span className="ui-text-sm ui-text-primary ui-flex">
-                {tooltip}
-              </span>
+              <span className="ui-text-sm ui-flex">{tooltip}</span>
             ) : (
               tooltip
             )}
           </span>
           <span className="ui-absolute ui-z-50 ui-h-2 ui-w-2 ui-flex ui-right-1/2 -ui-bottom-1 ui-left-1/2 -ui-translate-x-1/2">
-            <span className="ui-bg-base-content ui-h-full ui-w-full ui-flex ui-rotate-45" />
+            <span className="ui-bg-base-200 ui-h-full ui-w-full ui-flex ui-rotate-45" />
           </span>
         </span>
       ) : null}
