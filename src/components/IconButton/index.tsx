a79960d3ac470b5279ace1f4da4a8d5bc019e5b5
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2025-01-06 21:32:43 -0600

    feat: improve icon button

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,12 +1,9 @@
 import clsx, { ClassValue } from "clsx";
 import { HTMLProps, ReactNode, useState } from "react";
-import { ButtonType, ButtonVariant } from "../Button";
 
 export interface IconButtonProps
   extends Omit<HTMLProps<HTMLButtonElement>, "className" | "size" | "type"> {
   className?: ClassValue;
-  type?: ButtonType;
-  variant?: ButtonVariant;
   icon: ReactNode;
   tooltip?: ReactNode;
 }
@@ -15,6 +12,7 @@ export interface IconButtonProps
  * IconButton component
  * @param {className} className - The className to apply to the icon button.
  * @param {icon} icon - The icon to display in the button.
+ * @param {tooltip} tooltip - The tooltip to display when the button is hovered.
  */
 const IconButton = ({
   className,
@@ -25,33 +23,38 @@ const IconButton = ({
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div // NOSONAR
-      className="ui-relative"
+    <button
+      className={clsx(
+        "ui-relative ui-btn-ghost ui-rounded-full ui-w-fit ui-p-2 ui-transition-colors",
+        className
+      )}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      {...props}
     >
-      <button
-        className={clsx(
-          "ui-btn-ghost ui-relative ui-btn-circle ui-w-fit ui-h-fit ui-p-2 ui-transition-colors",
-          className
-        )}
-        {...props}
-      >
-        {icon}
-      </button>
+      {icon}
       {isHovered && tooltip ? (
-        <span className="ui-absolute ui-animate-scale-down ui-origin-bottom ui-truncate ui-py-2 ui-px-3 -ui-left-1/2 -ui-right-1/2 ui-bottom-full ui-items-center ui-justify-center ui-rounded-md ui-w-fit ui-shadow-sm ui-flex ui-bg-base-content">
-          {typeof tooltip === "string" ? (
-            <span className="ui-text-sm ui-text-primary ui-flex">
-              {tooltip}
-            </span>
-          ) : (
-            tooltip
-          )}
-          <span className="ui-absolute ui-bg-primary ui-w-2 ui-h-2" />
+        <span className="ui-absolute ui-left-1/2 ui-right-1/2 -ui-translate-x-1/2 ui-bottom-[calc(100%+0.5rem)] ui-w-fit">
+          <span
+            className={clsx(
+              "ui-bg-base-content ui-rounded-lg ui-truncate ui-py-2 ui-px-3 ui-w-fit ui-shadow-sm ui-h-fit",
+              "ui-items-center ui-justify-center ui-flex ui-relative"
+            )}
+          >
+            {typeof tooltip === "string" ? (
+              <span className="ui-text-sm ui-text-primary ui-flex">
+                {tooltip}
+              </span>
+            ) : (
+              tooltip
+            )}
+          </span>
+          <span className="ui-absolute ui-z-50 ui-h-2 ui-w-2 ui-flex ui-right-1/2 -ui-bottom-1 ui-left-1/2 -ui-translate-x-1/2">
+            <span className="ui-bg-base-content ui-h-full ui-w-full ui-flex ui-rotate-45" />
+          </span>
         </span>
       ) : null}
-    </div>
+    </button>
   );
 };
 
